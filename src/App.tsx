c0fb067591b32b646462d3c9e615b82a2b15c1df
Mohamed Elnafad <mohamedelnafad4@gmail.com>
2024-12-31 20:35:48 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Rate from './components/Stars.tsx';
+
+export const App = () => {
+  return (
+    <div className='container'>
+      <Rate />
+    </div>
+  );
+};
---
+++ src/components/Stars.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './start.css'; // Optional for styling
+
+const Rate = ({ totalItems = 5 }) => {
+  const [hoverIndex, setHoverIndex] = useState(null);
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const handleMouseEnter = (index) => {
+    setHoverIndex(index);
+  };
+
+  const handleMouseLeave = () => {
+    setHoverIndex(null);
+  };
+
+  const handleClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const handleTabKey = (event: any) => {
+    if (event.key === 'Tab') {
+      setSelectedIndex((prev: number) => prev + 1);
+    }
+  };
+  return (
+    <div className='rate-container'>
+      {Array.from({ length: totalItems }, (_, index) => (
+        <button
+          key={index}
+          className={`rate-item ${
+            hoverIndex !== null && index <= hoverIndex
+              ? 'hovered'
+              : selectedIndex !== null && index <= selectedIndex
+              ? 'selected'
+              : ''
+          }`}
+          onMouseEnter={() => handleMouseEnter(index)}
+          onMouseLeave={handleMouseLeave}
+          onClick={() => handleClick(index)}
+          onKeyDown={handleTabKey}
+        >
+          ★
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Rate;
